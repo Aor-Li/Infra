5a2367085d3593faf013fc742fc84dacd13c81bb
aor <liyifeng@philoai.com>
2026-08-04 20:00:13 +0800

    update ghostty settings

--- modules/aspects/trait/06-develope/shell/terminal/ghostty.nix
+++ modules/aspects/trait/06-develope/shell/terminal/ghostty.nix
@@ -2,28 +2,41 @@
 {
   den.aspects.dev.shell.terminal.ghostty = {
 
-    nixos = 
-      { pkgs, ... }:
-      {
-        systemPackages = [ pkgs.ghostty ];
-      };
+    nixos = { pkgs, ... }: {
+      systemPackages = [ pkgs.ghostty ];
+    };
 
     darwin = {
       homebrew.casks = [ "ghostty" ];
     };
 
-    homeManager =
-      { lib, pkgs, ... }:
-      {
-        programs.ghostty = {
-          enable = true;
-          package = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin null;
-          settings = {
-            theme = "Catppuccin Mocha";
-            font-family = "CaskaydiaCove Nerd Font";
-            shell-integration-features = "ssh-env,ssh-terminfo";
-          };
+    homeManager = { lib, pkgs, ... }: {
+      programs.ghostty = {
+        enable = true;
+        package = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin null;
+
+        # shell integration
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableFishIntegration = true;
+
+        settings = {
+          theme = "Catppuccin Mocha";
+          font-family = [
+            "CaskaydiaCove Nerd Font"
+            "等距更纱黑体 SC"
+          ];
+
+          # window and tabs
+          window-decoration = "auto";
+          macos-titlebar-style = "tabs";
+
+          # apply terminal info to shell integration
+          shell-integration-features = "ssh-env,ssh-terminfo";
         };
+
       };
+    };
+
   };
 }
